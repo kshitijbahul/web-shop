aec26e53e03a55e209d90654dd2e93ba97a52097
kshitijbahul <chunks.3@gmail.com>
2020-12-21 18:00:31 +0100

    Style to get Image hover effects

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,13 +4,21 @@ import './menu-item.style.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => {
     return (
-        <div style={{
-            backgroundImage : `url(${imageUrl})`
-        }} /** another way of adding css in the React element , 
-            Specially useful when it's a prop */
-        className={`${size} menu-item`}>
+        <div className={`${size} menu-item`}>
+                <div className='background-image' 
+                    /** 
+                     * another way of adding css in the 
+                     * React element , 
+                     * Specially useful when it's a prop 
+                     * */
+                    style={{
+                        backgroundImage : `url(${imageUrl})`
+                    }}
+                >
+
+                </div>
                 <div className='content'>
-                    <h1 className='title'>{title}</h1>
+                    <h1 className='title'>{title.toUpperCase()}</h1>
                     <span className="subtitle">SHOP NOW</span>
                 </div>
         </div>
